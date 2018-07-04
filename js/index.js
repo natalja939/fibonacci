d809c2e44de0f42637a0b577524645f76d01f2c6
NataliiaB <bejbanatalja939@gmail.com>
2018-07-04 18:34:16 +0000

    Edite js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
 //Loop
 
-//  let n = parseFloat(prompt('Enter number'));
+  let n = parseFloat(prompt('Enter number'));
 
-
-//   function fib(n) {
+  function fib(n) {
     
-//        let fibPrev  = 1,
-//            fibPrePrev = 1;
-//           for (let i = 1; i <=n; i++) {
-//             let fibNext = fibPrev + fibPrePrev;
-//                 fibPrePrev = fibPrev;
-//                 fibPrev = fibNext;
-//           }
-//                return fibPrev;
-//   }
-//  let result = fib(n);
-//  alert(result);
+       let fibPrev  = 1,
+           fibPrePrev = 1;
+          for (let i = 1; i <=n; i++) {
+             let fibNext = fibPrev + fibPrePrev;
+                 fibPrePrev = fibPrev;
+                 fibPrev = fibNext;
+           }
+                return fibPrev;
+   }
+ let result = fib(n);
+  alert(result);
 
 
 
@@ -32,16 +31,14 @@
 //  alert(result);
     
 //recursion
-    
-    
-    let n = parseFloat(prompt('Enter number'));
-    function fib(n) {
-        let fib;
-        if (n > 1) {
-            return n < 1 ? n : fib(n - 1) + fib(n - 2);
-        }else {
-		    return fib(n);
-	    }
-    }
-    let result = fib(n);
-    alert(result);
+//   let n = parseFloat(prompt('Enter number'));
+//    function fib(n) {
+//        let fib;
+//        if (n > 1) {
+//            return fib(n - 1) + fib(n - 2);
+//        }else {
+//		    return fib(n);
+//	    }
+//    }
+//    let result = fib(n);
+//    alert(result);
